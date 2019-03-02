2d8ed1871818cbf0e3c6488d61cb9d906decaf29
A.Maj <a.maj.maivs@gmail.com>
2019-03-02 14:12:34 +0100

    Initial commit

---
+++ src/main/java/com/bootcamp/bootcamp/controllers/AboutController.java
@@ -0,0 +1,9 @@
+package com.bootcamp.bootcamp.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AboutController {
+
+
+}
---
+++ src/main/java/com/bootcamp/bootcamp/controllers/HomeController.java
@@ -0,0 +1,34 @@
+package com.bootcamp.bootcamp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+//@RequestMapping(value = "/bootcamp", method = RequestMethod.GET) Użytaczne w przypadku gdy strony posiadają część wspólną w adresie.
+public class HomeController {
+
+    //@RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/")
+    public String getHome(@RequestParam(name = "imie", defaultValue = "Jan") String firstName,
+                          @RequestParam(name = "nazwisko", defaultValue = "Ktoś") String lastName,
+                          @RequestParam(defaultValue = "25") int wiek,
+                          Model model) {
+//        System.out.println("Witaj " + firstName + " " + lastName + " " + wiek,);
+//        if (wiek > 20) {
+//            return "redirect:o-nas";
+//            return "forward:o-nas"; nie aktualizuje adresu do ktorego uzytkownik jest przekierowywany. Wyswietla "home"
+//        }
+        model.addAttribute("nazwaZmiennejWwidoku", firstName);
+        model.addAttribute("lastName", lastName);
+        return "home";
+    }
+    //@RequestMapping(value = "/o-nas", method = RequestMethod.GET)
+    @GetMapping("/o-nas")
+    public String getAbout() {
+        return "about";
+    }
+}
