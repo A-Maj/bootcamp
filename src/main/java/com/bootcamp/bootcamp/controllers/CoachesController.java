e65371c1b9365c3962301c55cfbba8dd0031c641
A.Maj <a.maj.maivs@gmail.com>
2019-03-03 11:44:11 +0100

    Update2

--- src/main/java/com/bootcamp/bootcamp/controllers/CoachesController.java
+++ src/main/java/com/bootcamp/bootcamp/controllers/CoachesController.java
@@ -6,19 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/trenerzy")
 public class CoachesController {
 
     @Autowired
     private CoachesService coachesService;
 
-    @GetMapping("/nasi-trenerzy")
+    @GetMapping("")
     public String getCoaches(Model model) {
 
         model.addAttribute("coachesList", coachesService.getCoaches());
+
         System.out.println(coachesService.getCoaches());
         return "coaches";
+    }
+
+    //localhost:8080/trenerzy/{id}
+    //np. localhost:8080/trenerzy/1
+    @GetMapping("/{id}")
+    public String showCoach(@PathVariable(name = "id") int id, Model model) {
+        model.addAttribute("coach",id);
+        return "coach";
     }
 
 
--- src/main/java/com/bootcamp/bootcamp/controllers/ContactController.java
+++ src/main/java/com/bootcamp/bootcamp/controllers/ContactController.java
@@ -1,13 +1,27 @@
 package com.bootcamp.bootcamp.controllers;
 
+import com.bootcamp.bootcamp.model.Contact;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ContactController {
 
     @GetMapping("/kontakt")
-    public String getContact() {
+    public String getContact(Model model) {
+
+        model.addAttribute("contact", new Contact());
+        return "contact";
+    }
+
+    //@ModelAttribute odnosi się do całego modelu.
+    @PostMapping("/wyslij")
+    public String sent(@ModelAttribute Contact contact, Model model) {
+        model.addAttribute("isSent", true);
         return "contact";
     }
 }
